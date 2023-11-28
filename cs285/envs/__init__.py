6a86f0a03c401c8c8f9955ef21a569c14d402eb9
Zekai Wang <zekai.wang@berkeley.edu>
2023-11-28 15:19:44 -0800

    trying to figure out env

--- cs285/envs/__init__.py
+++ cs285/envs/__init__.py
@@ -16,3 +16,8 @@ def register_envs():
         entry_point='cs285.envs.reacher:Reacher7DOFEnv',
         max_episode_steps=500,
     )
+    register(
+        id='pendulum-cs285-v0',
+        entry_point='cs285.envs.pendulum:PendulumEnv',
+        max_episode_steps=1000,
+    )
---
+++ cs285/envs/pendulum/__init__.py
@@ -0,0 +1,1 @@
+from cs285.envs.pendulum.pendulum_env import PendulumEnv
